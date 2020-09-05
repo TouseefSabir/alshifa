5ee12b4eb0f54fde15ed22b22baf56ee49744cee
Touseef Sabir <touseefsabir41@gmail.com>
2020-09-06 02:52:32 +0500

    about discription added

--- includes/about.php
+++ includes/about.php
@@ -7,13 +7,8 @@
                          <div class="about-info">
                               <h2 class="wow fadeInUp" data-wow-delay="0.6s">WELCOME TO Al Shifa HIJAMA CUPPING THERAPY</h2>
                               <div class="wow fadeInUp" data-wow-delay="0.8s">
-                                   <p>Al Shifa Hijama & Ruqyah Centre is a health centre, the first of its kind in 57 Preston St, Brighton, East Sussex. BN1 2HE, London.
-                                        Here, Islamic tradition as well as technology are combined to offer the best health services according to the Quran, Prophetic and modern medicine.
-                                        Our state of the art health facility offers Hijama, Ruqyah and combinations of medical treatments prescribed by the Quran and Sunnah at affordable prices without having to compromise on quality.
-                                        Our team of qualified staff is led by Al shifa who has qualified from England,UK. He has been practising Ruqyah and Hijama for more than 9 years.
-                                        We also have highly qualified Islamic Scholars and Hufaz of the Quran who perform Hijama and Ruqyah.
-                                        Additionally, we have qualified female nurses who perform hijama for females.
-                                        We are also in contact with Islamic Scholars qualified from the Islamic University of Madinah who offer advice related to any fiqh (jurisprudence) matter.</p>
+                                   <p>Al Shifa Hijama Clinic is a health centre, the first of its kind in 57 Preston St, Brighton, East Sussex. BN1 2HE, London. Here, Islamic tradition as well as technology are combined to offer the best health services. Our state of the art health facility offers Hijama, Ruqyah and combinations of medical treatments at affordable prices without having to compromise on quality.
+                                    Our team of qualified staff is led by Mr Mohammad and Miss Shifa. He has been practising Hijama for more than 9 years. Additionally, we have a qualified female nurse who performs hijama for females.</p>
 
                               </div>
                               <figure class="profile wow fadeInUp" data-wow-delay="1s">
